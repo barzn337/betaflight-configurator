969596613360ef4a2e8da78b38086dd0fad4522e
barzn337 <123651030+barzn337@users.noreply.github.com>
2025-06-06 16:54:16 +0300

    Feat: Restore configurator behaviour for 3 PID Profiles and 6 Rate Profiles.

--- src/js/fc.js
+++ src/js/fc.js
@@ -27,8 +27,8 @@ const INITIAL_CONFIG = {
     craftName:                        '',
     displayName:                      '', // present for backwards compatibility before MSP v1.45
     pilotName:                        '',
-    pidProfileNames:                  ["", "", "", ""],
-    rateProfileNames:                 ["", "", "", ""],
+    pidProfileNames:                  ["", "", ""],
+    rateProfileNames:                 ["", "", "", "", "", ""],
     numProfiles:                      3,
     rateProfile:                      0,
     boardType:                        0,
